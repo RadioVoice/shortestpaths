694b948956303279644cfaa07a3915e02f3b9b8f
Daniel Kessler <djk172@case.edu>
2019-11-21 15:24:16 -0500

    Added Edge, SimpleEdge, and Route

--- src/Route.java
+++ src/Route.java
@@ -1,2 +1,38 @@
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Route {
+    private LinkedList<Node> nodes;
+    private LinkedList<Edge> edges;
+    private BigInteger totalCost;
+
+    public Route(){
+        nodes = new LinkedList<Node>();
+        edges = new LinkedList<Edge>();
+        totalCost = new BigInteger("0");
+    }
+
+    public void add(Node node){
+        Objects.requireNonNull(node);
+        nodes.add(node);
+    }
+
+    public void add(Edge edge){
+        Objects.requireNonNull(edge);
+        edges.add(edge);
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        while(index < nodes.size() && index < edges.size()){
+            sb.append(nodes.get(index));
+            sb.append(edges.get(index));
+        }
+        return sb.toString();
+    }
+
+
+
 }
--- src/SimpleEdge.java
+++ src/SimpleEdge.java
@@ -1,2 +1,45 @@
-public class SimpleEdge {
+import java.math.BigInteger;
+import java.security.InvalidParameterException;
+import java.util.Objects;
+
+public class SimpleEdge implements Edge{
+
+    private BigInteger id;
+    private BigInteger cost;
+    private Node origin;
+    private Node dest;
+
+    private SimpleEdge(BigInteger id, BigInteger cost, Node origin, Node dest){
+        this.id = id;
+        this.cost = cost;
+        this.origin = origin;
+        this.dest = dest;
+    }
+
+    public static SimpleEdge of(BigInteger id, BigInteger cost, Node origin, Node dest){
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(cost);
+        Objects.requireNonNull(origin);
+        Objects.requireNonNull(dest);
+        if (origin.equals(dest)){
+            throw new InvalidParameterException("Origin and Destination nodes must be different");
+        }
+        return new SimpleEdge(id, cost, origin, dest);
+    }
+
+    public BigInteger getID() {
+        return id;
+    }
+
+    public Node getOrigin() {
+        return origin;
+    }
+
+    public Node getDest() {
+        return dest;
+    }
+
+    public BigInteger getCost() {
+        return cost;
+    }
 }
