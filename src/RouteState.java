09621b7840f48139e98f5a3715bfc6d605b6aab2
Daniel Kessler <djk172@case.edu>
2019-11-22 12:21:39 -0500

    added RouteState

--- src/RouteState.java
+++ src/RouteState.java
@@ -1,18 +1,68 @@
-import javax.swing.*;
-import java.util.HashSet;
-import java.util.NavigableSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.math.BigInteger;
+import java.util.*;
 
-public class RouteState {
-    private Set<Node> nodeSet;
-    private NavigableSet<Node> unreached;
+final class RouteState {
+    private Map<Node, RouteNode> nodeMap ;
+    private final NavigableSet<RouteNode> unreached;
 
-    private RouteState(Set<Node> nodes, Node origin){
-        nodeSet = new HashSet<Node>();
-        unreached = new TreeSet<Node>();
+    private RouteState(Set<Node> nodes, Node origin, BigInteger startingCost){
 
-        //SimpleNode firstNode = SimpleNode.of()
+        nodeMap = new HashMap<Node, RouteNode>();
+        unreached = new TreeSet<RouteNode>();
 
+        RouteNode startingNode = RouteNode.of(origin, startingCost, null);
+
+        for(Node n : nodes) {
+            RouteNode temp = RouteNode.of(n);
+            nodeMap.put(n, temp);
+        }
+
+        this.replaceNode(startingNode);
+    }
+    static RouteState of(Set<Node> nodes, Node origin, BigInteger startingCost){
+        Objects.requireNonNull(nodes, "set is null");
+        Objects.requireNonNull(origin, "origin is null");
+        Objects.requireNonNull(startingCost, "starting cost is null");
+        return new RouteState(nodes, origin, startingCost);
+    }
+
+    //adds or replaces the node in unreached; just replaces in airportNodes
+    final void replaceNode(RouteNode routeNode){
+        Objects.requireNonNull(routeNode, "route node is null");
+
+        Node node = routeNode.getNode();
+        assert (node != null);
+
+        RouteNode oldNode = nodeMap.get(node);
+        assert (oldNode != null);
+
+        if(unreached.contains(oldNode))
+            unreached.remove(oldNode);
+        unreached.add(routeNode);
+        nodeMap.replace(node, routeNode);
+    }
+
+    final boolean allReached(){
+        return unreached.isEmpty();
+    }
+
+    final RouteNode closestUnreached() {
+        if (allReached()) {
+            throw new NoSuchElementException("all nodes have been reached");
+        }
+        return unreached.first();
+    }
+
+    final RouteNode associatedRouteNode(Node node){
+        Objects.requireNonNull(node, "node is null");
+        if(nodeMap.containsKey(node)){
+            return nodeMap.get(node);
+        }
+        return null;
+    }
+
+    final void reached(RouteNode routeNode){
+        Objects.requireNonNull(routeNode, "route node is null");
+        unreached.remove(routeNode);
     }
 }
