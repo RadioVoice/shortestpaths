c0d1aebcbbca465fd152d5a4980680f91ef3e0d8
Jack Bilotti <jtb129@case.edu>
2019-11-22 11:45:57 -0500

    SimpleNode.java

---
+++ src/SimpleNodeTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.*;
+
+
+public class SimpleNodeTest {
+
+    @Test
+    public void SimpleNodeTest() {
+        //Code Coverage
+        BigInteger id = new BigInteger("10001");
+
+
+    }
+}
