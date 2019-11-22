8d8a27494b27597227739ef809537d932fc16ace
Jack Bilotti <jtb129@case.edu>
2019-11-22 10:09:52 -0500

    Node and SimpleNode.java

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,12 @@
+import java.math.BigInteger;
+import java.util.LinkedList;
+
 public interface Node {
+
+    abstract BigInteger getID();
+    abstract LinkedList<Edge> getEdges();
+    abstract BigInteger getCost();
+    abstract boolean addEdge();
+    abstract boolean removeEdge();
+
 }
--- src/SimpleNode.java
+++ src/SimpleNode.java
@@ -1,2 +1,41 @@
-public class SimpleNode {
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class SimpleNode implements Node {
+
+    private BigInteger id;
+    private LinkedList<Edge> edges;
+    private BigInteger cost;
+    private Node previous;
+
+    private SimpleNode(BigInteger id, LinkedList<Edge> edges, BigInteger cost, Node previous) {
+        this.id = id;
+        this.edges = edges;
+        this.cost = cost;
+        this.previous = previous;
+    }
+
+    public SimpleNode of(BigInteger id, LinkedList<Edge> edges, BigInteger cost, Node previous) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(edges);
+        Objects.requireNonNull(cost);
+        Objects.requireNonNull(previous);
+
+        //Check if edges is empty???
+        return new SimpleNode(id, edges, cost, previous);
+    }
+
+    public BigInteger getId() { return id; }
+
+    public LinkedList<Edge> getEdges() { return edges; }
+
+    public BigInteger getCost() { return cost; }
+
+    public Node getPrevious() { return previous; }
+
+    public boolean equals() { }
+
+    public int compareTo() {}
+
 }
